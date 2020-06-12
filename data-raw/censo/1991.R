8bc298cc95ad94b762038adfc0eb33c4f5393b9a
Alberson Miranda <45690517+albersonmiranda@users.noreply.github.com>
2020-06-11 21:14:34 -0300

    Adicionando data-raw.

---
+++ data-raw/censo/1991.R
@@ -0,0 +1,64 @@
+### CENSO 1991 ###
+
+
+# O objetivo desse projeto é obter e visualizar
+# a relação de pobreza e raça nos municípios do Brasil.
+#
+# Os dados foram obtidos em http://tabnet.datasus.gov.br/cgi/ibge/censo/rendadescr.htm
+
+
+# 1. PACOTES ----
+
+
+library(tidyverse)
+
+
+# 2. IMPORT ----
+
+
+# dados do IBGE
+censo_1991 = foreign::read.dbf("data-raw/censo/RENDABR91.dbf")
+censo_1991 = censo_1991 %>%
+  mutate(across(where(is.integer), as.numeric))
+
+# verificando último ano disponível
+ult = geobr::list_geobr() %>%
+  filter(geography == "Municipality") %>%
+  mutate(years = str_split(years, ", ")) %>%
+  select(years) %>%
+  unlist()
+
+ult = as.numeric(ult[length(ult)][[1]])
+
+# mapas
+mapa = geobr::read_municipality(code_muni="all", year=ult) %>%
+  mutate(code_muni = str_sub(code_muni, 1, 6))
+
+
+# 3. DATA CLEANING ----
+
+
+censo_1991 = censo_1991 %>% inner_join(mapa, by = c("MUNCOD" = "code_muni")) %>%
+  mutate(CORRACA = case_when(CORRACA == 1 ~ "BRANCO",
+                             CORRACA == 2 ~ "PRETO",
+                             CORRACA == 3 ~ "AMARELO",
+                             CORRACA == 4 ~ "PARDO",
+                             CORRACA == 5 ~ "INDIGENA",
+                             CORRACA == 0 ~ "SEM DECLARACAO"),
+         RM = NUMRENDA / DENRENDA,
+         DSMPRG = NUMDESOCUP / DENDESOCUP) %>%
+  select(abbrev_state, name_muni, CORRACA,
+         NUMRENDA, DENRENDA, RM,
+         NUMPOBRES, NUMPOBRESX,
+         NUMDESOCUP, DENDESOCUP, DSMPRG,
+         geom) %>%
+  pivot_wider(id_cols = c(abbrev_state, name_muni, geom),
+              names_from = CORRACA,
+              values_from = c(NUMRENDA, DENRENDA, RM,
+                              NUMPOBRES, NUMPOBRESX,
+                              NUMDESOCUP, DENDESOCUP, DSMPRG))
+
+# 4. DATA ----
+
+
+usethis::use_data(censo_1991, overwrite = TRUE)
---
+++ data-raw/lintr.R
@@ -0,0 +1,2 @@
+lintr::lint_package(linters = lintr::with_defaults(assignment_linter = NULL),
+                    exclusions = list("R/data_2010.R"))
